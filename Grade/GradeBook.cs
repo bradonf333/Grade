f15cf5f5a2231b3c2a18e19af10e3a2b8585890d
Bradon Fredrickson <bradonf123@gmail.com>
2017-03-02 21:28:44 -0700

    Add project files.

---
+++ Grade/GradeBook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade
+{
+    class GradeBook
+    {
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            return new GradeStatistics();
+        }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        private List<float> grades;
+    }
+}
