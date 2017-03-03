2834037456c2650c024996c06b858898733e1b9a
Bradon Fredrickson <bradonf123@gmail.com>
2017-03-03 16:12:40 -0700

    Entered Unit tests for gradebook

--- Grade/GradeBook.cs
+++ Grade/GradeBook.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Grade
 {
-    class GradeBook
+    public class GradeBook
     {
         public GradeBook()
         {
@@ -33,6 +33,8 @@ namespace Grade
         {
             grades.Add(grade);
         }
+
+        public string Name;
 
         private List<float> grades;
     }
--- Grade/GradeStatistics.cs
+++ Grade/GradeStatistics.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Grade
 {
-    class GradeStatistics
+    public class GradeStatistics
     {
 
         public GradeStatistics()
