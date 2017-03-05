a405e977165152637305cde7e8c76b812f4f3e45
Bradon Fredrickson <bradonf123@gmail.com>
2017-03-05 16:20:36 -0700

    Changed my comments about the event into the summary tags.

--- Grade/NameChangedDelegate.cs
+++ Grade/NameChangedDelegate.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 
 namespace Grade
 {
-    /* Conventional Way or the correct way
-     * An event always passes 2 parameters:
-     * 1 - The sender of the event. Send itself.
-     * 2 - All the arguments or all the needed info about the event.
-     * This requires a custom class to put together all the needed info into an object 
-     * which can then be passed as the arguments
-     */
+     /// <summary>
+     /// An event always passes 2 parameters:
+     /// 1 - The sender of the event. Send itself.
+     /// 2 - All the arguments or all the needed info about the event.
+     /// This requires a custom class to put together all the needed info into an object
+     /// which can then be passed as the arguments
+     /// </summary>
+     /// <param name="sender"></param>
+     /// <param name="args"></param>
     public delegate void NameChangedDelegate(object sender, NameChangedEventArgs args);
 }
