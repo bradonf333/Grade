537f1d4057676674d351812f94a4a4f65c1f5506
Bradon Fredrickson <bradonf123@gmail.com>
2017-03-08 18:43:09 -0700

    Added ComputeStatistics method within the ThrowAwayGradeBook class

--- Grade/ThrowAwayGradeBook.cs
+++ Grade/ThrowAwayGradeBook.cs
@@ -3,8 +3,13 @@ namespace Grade
 {
 	public class ThrowAwayGradeBook : GradeBook
 	{
-		public ThrowAwayGradeBook()
+		public GradeStatistics ComputeStatistics()
 		{
+
+
+			// Base reaches the inherited class information.
+			// This means I am accessing the GradeBook.ComputeStatistics not the ThrowAwayGradeBook.ComputeStatistics
+			return base.ComputeStatistics();
 		}
 	}
 }
